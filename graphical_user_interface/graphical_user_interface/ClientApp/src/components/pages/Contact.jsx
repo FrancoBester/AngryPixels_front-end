0c328eb1c22d3866f6bb695d32c795357987cb0c
iamkeryn <kerynroos1501@gmail.com>
2021-10-02 12:27:01 +0200

    contact
    - still having styling issues

--- graphical_user_interface/graphical_user_interface/ClientApp/src/components/pages/Contact.jsx
+++ graphical_user_interface/graphical_user_interface/ClientApp/src/components/pages/Contact.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 import './Contact.css';
 
 
+
 function Contact() {
   return (
-    <div class="grid">
+    <div class="gridContact">
       <header className="contactUsHeader">Contact Us</header>
       <main className="leftSideAbout">        
-        <input className="contactInput" type="text" placeholder="Your Name..."/>
-        <input type="email" placeholder="Email..."/>
-        <input type="text" placeholder="Message..."/>
-        <button className="btnSend">Send</button>        
-      </main>
-
-      <aside className="rightSideAbout">
-        <i className="fas fa-map-marker-alt fa-lg" ></i><h4>Vanderbijylpark, South Africa</h4>
+      <i className="fas fa-map-marker-alt fa-lg" ></i><h4>Vanderbijylpark, South Africa</h4>
         <br />
         <i className="fas fa-phone-alt fa-lg"></i><h4>011 435 8893</h4>
         <br />
         <i className="fas fa-envelope fa-lg"></i><h4>meditrust@gmail.com</h4>
         <br />
+       
+      </main>
+
+      <aside className="rightSideAbout">
+       
 
         <h4>Follow us on Social Media:</h4>
             <a href="https://www.facebook.com/"><i className="fab fa-facebook-square fa-3x center" id="faIcon"></i></a>

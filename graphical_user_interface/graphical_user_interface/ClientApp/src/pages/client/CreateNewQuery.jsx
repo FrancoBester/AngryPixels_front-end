875d9a3a653d688550c7e13b56b6d81c78f2a100
iamkeryn <kerynroos1501@gmail.com>
2021-10-12 22:00:11 +0200

    CreateNewQuery - still trying to get it working

--- graphical_user_interface/graphical_user_interface/ClientApp/src/pages/client/CreateNewQuery.jsx
+++ graphical_user_interface/graphical_user_interface/ClientApp/src/pages/client/CreateNewQuery.jsx
@@ -1,27 +1,55 @@
 import React from 'react';
 import './CreateNewQuery.css';
 import { useHistory } from "react-router";
+import { useState } from "react";
+import API from "../../API";
 
 
 function CreateNewQuery() {
 
     const history = useHistory();
+    const [QueryTitle, setQueryTitle] = useState("");
+    const [QueryLevel, setQueryLevel] = useState("");
+    const [QueryDescription, setQueryDescription] = useState("");
+
+    function PostNewQuery(){
+        const queryObj = {
+            QueryTitle: QueryTitle,
+            QueryLevel: QueryLevel,
+            QueryDescription: QueryDescription
+        };
+
+        API.APIPOST(
+            "Queries/CreateQuery",
+            queryObj,
+            () => {alert("Success")},
+            () => {alert("Error")},
+            () => {}
+        );
+        history.push("/Client");
+    }
 
     return (
         <>
         <div className="newQueryGrid">
-            <header className="createQueryHeader">Create New Query</header>
-            <div className="logQueryHeader">
-                <h2>To log a query, please provide us with the following information:</h2>
-            </div>
-            <main className="createQueryMain">
-
+            <div className="titleContainerQueries">
+                <header className="createQueryHeader">Create Query</header>
+            <form
+                onSubmit={() =>{
+                    PostNewQuery();
+                }}
+            >
                 <div className="rowQuey">
                     <div className="queryColLeft">
                         <label>Title:</label>
                     </div>
                     <div className="queryColRight">
-                        <input type="text" name="" id="" />
+                        <input
+                            type="text" 
+                            placeholder="Title"
+                            required
+                            onChange={(e) => setQueryTitle(e.target.value)}    
+                        />
                     </div>
                 </div>
 
@@ -30,28 +58,34 @@ function CreateNewQuery() {
                         <label>Level:</label>
                     </div>
                     <div className="queryColRight">
-                        <input type="text" placeholder="1 - Least Urgent, 3 - Very Urgent"/>
+                        <input 
+                            type="text" 
+                            placeholder="1 = Low Priority, 2 = Medium Priority, 3 = High Priority"
+                            required
+                            onChange={(e) => setQueryLevel(e.target.value)}
+                        />
                     </div>
                 </div>
-
 
                 <div className="rowQuey">
                     <div className="queryColLeft">
                         <label>Description:</label>
                     </div>
                     <div className="queryColRight">
-                        <input type="text" name="" id="" />
+                        <input 
+                            type="text" 
+                            placeholder="Give us more info about the issue..."
+                            required
+                            onChange={(e) => setQueryDescription(e.target.value)}    
+                        />
                     </div>
                 </div>
 
-                <button
+                <input
                     className="btnCreateQuery"
-                    onClick={() => {
-                        history.push();
-                    }}
-                >
-                    Create
-                </button>
+                    type="submit"
+                    value="Post"
+                />
 
                 <button
                     className="btnCancelCreateQuery"
@@ -61,8 +95,8 @@ function CreateNewQuery() {
                 >
                     Cancel
                 </button>
-            </main>
-
+            </form>
+            </div>
         </div>
         </>
     )

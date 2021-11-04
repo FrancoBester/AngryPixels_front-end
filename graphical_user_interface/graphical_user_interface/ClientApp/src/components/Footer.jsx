cc374c5dd26de1a928cf837502d2d2ba7d141245
Keryn Roos <kerynroos1501@gmail.com>
2021-11-04 18:40:24 +0200

    footer changed

--- graphical_user_interface/graphical_user_interface/ClientApp/src/components/Footer.jsx
+++ graphical_user_interface/graphical_user_interface/ClientApp/src/components/Footer.jsx
@@ -8,54 +8,32 @@ function Footer() {
     <div className="footer-container">
       <section className="footer-subscription">
         <p className="footer-subscription-heading">
-          Subscribe to our monthly newsletter
+         
         </p>
         <p className="footer-subscription-text">
-          You can unsubscribe at any time.
+          
         </p>
         <div className="input-areas">
           <form>
-            <input
-              className="footer-input"
-              name="email"
-              type="email"
-              placeholder="Your Email"
-            />
-            <Button buttonStyle="btn--outline">Subscribe</Button>
+           
           </form>
         </div>
       </section>
       <div className="footer-links">
         <div className="footer-link-wrapper">
           <div className="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
+            
           </div>
           <div className="footer-link-items">
-            <h2>Contact Us</h2>
-            <Link to="/">Contact</Link>
-            <Link to="/">Support</Link>
-            <Link to="/">FAQ's</Link>
+            
           </div>
         </div>
         <div className="footer-link-wrapper">
           <div className="footer-link-items">
-            <h2>Documents</h2>
-            <Link to="/">Claim Forms</Link>
-            <Link to="/">Application Forms</Link>
-            <Link to="/">Reference Document</Link>
-            <Link to="/">Appointment Form</Link>
+            
           </div>
           <div className="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
+            
           </div>
         </div>
       </div>
@@ -71,11 +49,13 @@ function Footer() {
           <div className="social-icons">
             <Link
               className="social-icon-link facebook"
-              to="/"
               target="_blank"
               aria-label="Facebook"
             >
-              <i className="fab fa-facebook-f" />
+              
+              <a href="https://www.google.com/url?q=https://www.facebook.com/meditrust.biz&sa=D&source=docs&ust=1636044097172000&usg=AOvVaw0jF_1iALl12eX_VWaOQBu9"
+              className="fab fa-facebook-f"
+              ></a>
             </Link>
             <Link
               className="social-icon-link instagram"
@@ -83,7 +63,9 @@ function Footer() {
               target="_blank"
               aria-label="Instagram"
             >
-              <i className="fab fa-instagram" />
+              <a href="https://www.instagram.com/"
+              className="fab fa-instagram"
+              ></a>
             </Link>
             <Link
               className="social-icon-link youtube"
@@ -91,7 +73,9 @@ function Footer() {
               target="_blank"
               aria-label="Youtube"
             >
-              <i className="fab fa-youtube" />
+              <a href="https://www.youtube.com/"
+              className="fab fa-youtube"
+              ></a>
             </Link>
             <Link
               className="social-icon-link twitter"
@@ -99,7 +83,9 @@ function Footer() {
               target="_blank"
               aria-label="Twitter"
             >
-              <i className="fab fa-twitter" />
+              <a href="https://twitter.com/"
+              className="fab fa-twitter"
+              ></a>
             </Link>
             <Link
               className="social-icon-link twitter"
@@ -107,7 +93,9 @@ function Footer() {
               target="_blank"
               aria-label="LinkedIn"
             >
-              <i className="fab fa-linkedin" />
+              <a href="https://www.linkedin.com/"
+              className="fab fa-linkedin"
+              ></a>
             </Link>
           </div>
         </div>

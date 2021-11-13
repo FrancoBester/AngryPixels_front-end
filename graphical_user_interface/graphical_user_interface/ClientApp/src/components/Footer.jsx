b2c560c6c22cb78c7fa2c9fa0cf4e48b205c99cc
Carel Haasbroek <84836107+CarelTransformIt@users.noreply.github.com>
2021-11-13 19:35:10 +0200

    Social buttons fixed

--- graphical_user_interface/graphical_user_interface/ClientApp/src/components/Footer.jsx
+++ graphical_user_interface/graphical_user_interface/ClientApp/src/components/Footer.jsx
@@ -7,34 +7,20 @@ function Footer() {
   return (
     <div className="footer-container">
       <section className="footer-subscription">
-        <p className="footer-subscription-heading">
-         
-        </p>
-        <p className="footer-subscription-text">
-          
-        </p>
+        <p className="footer-subscription-heading"></p>
+        <p className="footer-subscription-text"></p>
         <div className="input-areas">
-          <form>
-           
-          </form>
+          <form></form>
         </div>
       </section>
       <div className="footer-links">
         <div className="footer-link-wrapper">
-          <div className="footer-link-items">
-            
-          </div>
-          <div className="footer-link-items">
-            
-          </div>
+          <div className="footer-link-items"></div>
+          <div className="footer-link-items"></div>
         </div>
         <div className="footer-link-wrapper">
-          <div className="footer-link-items">
-            
-          </div>
-          <div className="footer-link-items">
-            
-          </div>
+          <div className="footer-link-items"></div>
+          <div className="footer-link-items"></div>
         </div>
       </div>
       <section className="social-media">
@@ -47,56 +33,27 @@ function Footer() {
           </div>
           <small className="website-rights">AngryPixels © 2021</small>
           <div className="social-icons">
-            <Link
-              className="social-icon-link facebook"
-              target="_blank"
-              aria-label="Facebook"
-            >
-              
-              <a href="https://web.facebook.com/meditrust.biz?_rdc=1&_rdr"
-              className="fab fa-facebook-f"
+            <span>
+              <a
+                href="https://web.facebook.com/meditrust.biz?_rdc=1&_rdr"
+                className="fab fa-facebook-f"
+                target="_blank"
               ></a>
-            </Link>
-            <Link
-              className="social-icon-link instagram"
-              to="/"
-              target="_blank"
-              aria-label="Instagram"
-            >
-              <a href="https://www.instagram.com/"
-              className="fab fa-instagram"
+            </span>
+            <span>
+              <a
+                href="https://www.instagram.com/"
+                className="fab fa-instagram"
+                target="_blank"
               ></a>
-            </Link>
-            {/* <Link
-              className="social-icon-link youtube"
-              to="/"
-              target="_blank"
-              aria-label="Youtube"
-            >
-              <a href="https://www.youtube.com/"
-              className="fab fa-youtube"
+            </span>
+            <span>
+              <a
+                href="https://twitter.com/meditrust_biz?lang=en"
+                className="fab fa-twitter"
+                target="_blank"
               ></a>
-            </Link> */}
-            <Link
-              className="social-icon-link twitter"
-              to="/"
-              target="_blank"
-              aria-label="Twitter"
-            >
-              <a href="https://twitter.com/meditrust_biz?lang=en"
-              className="fab fa-twitter"
-              ></a>
-            </Link>
-            {/* <Link
-              className="social-icon-link twitter"
-              to="/"
-              target="_blank"
-              aria-label="LinkedIn"
-            >
-              <a href="https://www.linkedin.com/"
-              className="fab fa-linkedin"
-              ></a>
-            </Link> */}
+            </span>
           </div>
         </div>
       </section>

ad63437d5aa3a998199f60b84fda86b4513fab59
Carel Haasbroek <84836107+CarelTransformIt@users.noreply.github.com>
2021-11-14 10:39:03 +0200

    Fixed the signup

--- graphical_user_interface/graphical_user_interface/ClientApp/src/pages/globalPages/Signup.jsx
+++ graphical_user_interface/graphical_user_interface/ClientApp/src/pages/globalPages/Signup.jsx
@@ -5,8 +5,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Signup.css";
 import API from "../../API";
-import Footer from '../../components/Footer';
-
+import Footer from "../../components/Footer";
 
 function Signup() {
   const [FirstName, setFirstName] = useState("");
@@ -45,215 +44,220 @@ function Signup() {
       "Users/Register",
       userObj,
       () => {
-        alert("You have succesfully created your account.")//This being weird, does not work if a move history.push to here
+        alert("You have succesfully created your account.");
+        history.push("/login");
       },
       () => {},
       () => {}
     );
-    history.push("/login");
   }
   return (
     <div>
       <div className="form-container-signup">
         <div className="title_container">
           <h1>Register</h1>
-          <form
-            onSubmit={() => {
-              Register();
-            }}
-          >
-            <div className="row">
-              <div className="col-25">
-                <label htmlFor="fname">First Name:</label>
-              </div>
-              <div className="col-75">
+
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="fname">First Name:</label>
+            </div>
+            <div className="col-75">
+              <input
+                type="text"
+                placeholder="First Name"
+                required
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-25">
+              <label>Last Name:</label>
+            </div>
+            <div className="col-75">
+              <input
+                type="text"
+                placeholder="Surname"
+                required
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-25">
+              <label>Email:</label>
+            </div>
+            <div className="col-75">
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-25">
+              <label>Phone Number:</label>
+            </div>
+            <div className="col-75">
+              <input
+                type="tel"
+                placeholder="071111111"
+                pattern="^(\+27|0)[6-8][0-9]{8}$"
+                required
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-25">
+              <label>Date Of Birth:</label>
+            </div>
+            <div className="col-75">
+              <DatePicker
+                selected={DateOfBirth}
+                onChange={(date) => setDateOfBirth(date)}
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="id">ID number:</label>
+            </div>
+            <div className="col-75">
+              <input
+                type="text"
+                placeholder="0000000000000"
+                required
+                onChange={(e) => setCountryId(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-25">
+              <label>Gender:</label>
+            </div>
+            <div className="col-75">
+              <label className="lblGender" for="Male">
                 <input
-                  type="text"
-                  placeholder="First Name"
-                  required
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-25">
-                <label>Last Name:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="text"
-                  placeholder="Surname"
-                  required
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-25">
-                <label>Email:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="email"
-                  placeholder="Email"
-                  required
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-25">
-                <label>Phone Number:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="tel"
-                  placeholder="071111111"
-                  pattern="^(\+27|0)[6-8][0-9]{8}$"
-                  required
-                  onChange={(e) => setPhoneNumber(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-25">
-                <label>Date Of Birth:</label>
-              </div>
-              <div className="col-75">
-                <DatePicker
-                  selected={DateOfBirth}
-                  onChange={(date) => setDateOfBirth(date)}
-                />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-25">
-                <label htmlFor="id">ID number:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="text"
-                  placeholder="0000000000000"
-                  required
-                  onChange={(e) => setCountryId(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-25">
-                <label>Gender:</label>
-              </div>
-              <div className="col-75">
-                
-                <label className="lblGender" for="Male"><input
                   onChange={(e) => setGender(e.target.value)}
                   type="radio"
                   value="Male"
                   name="genderChoice"
                   id="Male"
                 ></input>
-                Male</label>
-               
-                <label className="lblGender" for="Female"> <input
+                Male
+              </label>
+
+              <label className="lblGender" for="Female">
+                {" "}
+                <input
                   onChange={(e) => setGender(e.target.value)}
                   type="radio"
                   value="Female"
                   name="genderChoice"
                   id="Femeale"
                 ></input>
-                Female</label>
-
-              </div>
+                Female
+              </label>
             </div>
+          </div>
 
-            <div className="row">
-              <div className="col-25">
-                <label>City:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="text"
-                  placeholder="City"
-                  required
-                  onChange={(e) => setCity(e.target.value)}
-                />
-              </div>
+          <div className="row">
+            <div className="col-25">
+              <label>City:</label>
             </div>
+            <div className="col-75">
+              <input
+                type="text"
+                placeholder="City"
+                required
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
+          </div>
 
-            <div className="row">
-              <div className="col-25">
-                <label>Street Address:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="text"
-                  placeholder="Street"
-                  required
-                  onChange={(e) => setStreet(e.target.value)}
-                />
-              </div>
+          <div className="row">
+            <div className="col-25">
+              <label>Street Address:</label>
             </div>
+            <div className="col-75">
+              <input
+                type="text"
+                placeholder="Street"
+                required
+                onChange={(e) => setStreet(e.target.value)}
+              />
+            </div>
+          </div>
 
-            <div className="row">
-              <div className="col-25">
-                <label>Address Number:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="number"
-                  placeholder="Number"
-                  required
-                  onChange={(e) => setHouseNumber(e.target.value)}
-                />
-              </div>
+          <div className="row">
+            <div className="col-25">
+              <label>Address Number:</label>
             </div>
+            <div className="col-75">
+              <input
+                type="number"
+                placeholder="Number"
+                required
+                onChange={(e) => setHouseNumber(e.target.value)}
+              />
+            </div>
+          </div>
 
-            <div className="row">
-              <div className="col-25">
-                <label>Password:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="password"
-                  placeholder="Password"
-                  required
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
+          <div className="row">
+            <div className="col-25">
+              <label>Password:</label>
             </div>
+            <div className="col-75">
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
 
-            <div className="row">
-              <div className="col-25">
-                <label>Confirm Password:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="password"
-                  placeholder="Re-enter Password"
-                  required
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </div>
+          <div className="row">
+            <div className="col-25">
+              <label>Confirm Password:</label>
             </div>
-            <input className="btnSub" type="submit" value="Register" />
-            <button
-              className="btnCancel"
-              onClick={() => {
-                history.push("/");
-              }}
-            >
-              Cancel
-            </button>
-          </form>
+            <div className="col-75">
+              <input
+                type="password"
+                placeholder="Re-enter Password"
+                required
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <button
+            className="btnSub"
+            onClick={() => {
+              Register();
+            }}
+          >
+            Register
+          </button>
+          <button
+            className="btnCancel"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            Cancel
+          </button>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }

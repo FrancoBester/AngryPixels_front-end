29e56c9cf173912943cc1b9cb6ebf8d03849e79a
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2021-11-16 23:40:36 +0200

    changes some links

--- graphical_user_interface/graphical_user_interface/ClientApp/src/pages/globalPages/Contact.jsx
+++ graphical_user_interface/graphical_user_interface/ClientApp/src/pages/globalPages/Contact.jsx
@@ -15,7 +15,7 @@ function Contact() {
           <h4 className="leftConatctH4">Please dont hesitate to contact us for more information:</h4>
           </li>
           <li>            
-            <label for="locationIcon">Vanderbijlpark, South Africa</label>
+            <label for="locationIcon"><a href="https://www.google.com/maps?client=firefox-b-d&q=meditrust+south+africa&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiR2faq6530AhWDmFwKHSAACa0Q_AUoAXoECAEQAw" target="_blank" rel="noopener noreferrer">Vanderbijlpark, South Africa</a></label>
             <i className="fas fa-map-marker-alt fa-lg" id="locationIcon"></i>
           </li>
         <li>       
@@ -40,7 +40,7 @@ function Contact() {
           </li>
           <li>
             <i className="fab fa-facebook-square" id="fbIcon"></i>
-            <label for="fbIcon" ><a href="https://web.facebook.com/meditrust.biz?_rdc=1&_rdr">Facebook</a></label>
+            <label for="fbIcon" ><a href="https://web.facebook.com/meditrust.biz?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer">Facebook</a></label>
           </li>
           
           {/* <li>
@@ -49,11 +49,11 @@ function Contact() {
           </li> */}
           <li>
             <i className="fab fa-twitter-square" id="twitterIcon"></i>
-            <label for="twitterIcon"><a href="https://twitter.com/meditrust_biz?lang=en">Twitter</a></label>
+            <label for="twitterIcon"><a href="https://twitter.com/meditrust_biz?lang=en" target="_blank" rel="noopener noreferrer">Twitter</a></label>
           </li>          
            <li>
             <i className="fab fa-instagram" id="instaIcon"></i>
-            <label for="instaIcon"><a href="https://www.instagram.com/">Instagram</a></label>
+            <label for="instaIcon"><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">Instagram</a></label>
           </li>
         </ul>
       </aside>
